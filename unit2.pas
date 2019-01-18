09469a81da554ad2e0136931f22878443fa26050
4eburashk <csn@net4me.net>
2019-01-18 20:04:33 +0300

    поправочка файлов

---
+++ unit2.pas
@@ -0,0 +1,169 @@
+unit unit2;
+// приложуха с напоминанием дел.
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, strutils, LazUTF8, lclintf, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Button9: TButton;
+    Edit1: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Memo1: TMemo;
+    OpenDialog1: TOpenDialog;
+    SaveDialog1: TSaveDialog;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure Button6Click(Sender: TObject);
+    procedure Button7Click(Sender: TObject);
+    procedure Button8Click(Sender: TObject);
+    procedure Button9Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+    FirstTime: Boolean;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+// Функция FindInMemo: Возвращает позицию найденной строки
+function FindInMemo(AMemo: TMemo; AString: String; StartPos: Integer): Integer;
+begin
+  Result := PosEx(AString, AMemo.Text, StartPos);
+  if Result > 0 then
+  begin
+    AMemo.SelStart := UTF8Length(PChar(AMemo.Text), Result - 1);
+    AMemo.SelLength := Length(AString);
+   // AMemo.SetFocus;
+  end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+if FileExists('TODO.txt') then
+ Memo1.Lines.LoadFromFile('TODO.txt')
+else
+ Memo1.Lines.AddText('Файл TODO.txt не был найден в домашнем каталоге.'+LineEnding+'После правки списка, сохраните его как TODO.txt и он будет показываться автоматически при старте программы.'+LineEnding+'Предлагаю начать работу с кнопки "нумерация жёстко".');
+end;
+
+procedure TForm1.Button3Click(Sender: TObject); // EXIT
+begin
+  Form1.Close();
+end;
+
+procedure TForm1.Button4Click(Sender: TObject); // OPEN
+begin
+     if OpenDialog1.Execute then
+     Memo1.Lines.LoadFromFile(OpenDialog1.FileName);
+end;
+
+procedure TForm1.Button2Click(Sender: TObject); // CLOSE
+begin
+     if SaveDialog1.Execute then
+     Memo1.Lines.SaveToFile(SaveDialog1.FileName);
+end;
+
+procedure TForm1.Button1Click(Sender: TObject); // hard add list
+var
+  i: Integer;
+begin
+  //Memo1.Font.Color:=clRed;
+  for i := 1 to 9 do
+    Memo1.Lines[i-1]:=IntToStr(i)+'. ';
+  //Memo1.Font.Color:=clDefault;
+end;
+
+procedure TForm1.Button5Click(Sender: TObject); // soft add list
+var
+  j: Integer;
+begin
+  for j := 1 to 9 do
+    Memo1.Lines.Add(IntToStr(j)+'. ');
+end;
+
+procedure TForm1.Button7Click(Sender: TObject); // find text 
+const
+  SearchStr: String = '';                     // Строка, которую ищем
+  SearchStart: Integer = 0;                   // Позиция, с которой начинаем искать строку
+begin
+  if SearchStr <> Edit1.Text then begin
+    SearchStart := 0;
+    SearchStr := Edit1.Text;
+  end;
+  SearchStart := FindInMemo(Memo1, SearchStr, SearchStart + 1);
+
+  if SearchStart > 0 then
+    Label2.Caption := 'позиция['+IntToStr(SearchStart)+']'
+  else
+    Label2.Caption := 'не найдено';
+  Button7.SetFocus;
+end;
+
+procedure TForm1.Button8Click(Sender: TObject); // Убрать готовое. Строки, начинающиеся с + удалить.
+// недоделка: При удалении строк, мы меняем сам массив с которым работаем. И соответственно, могут быть глюки.
+// подумать как переделать. Скорее всего, сначала запомнить нужные строки, а потом хором удалить.
+var
+  cnt, i : Integer;
+  //n:=0: Integer; // идентификатор в массиве.
+  //arr: Array of Integer; // для записи номеров удаляемых строк.
+begin
+  cnt := Memo1.Lines.Count -1; 
+  for i:=0 to cnt do // цикл по массиву TMemo с нуля, а номера строк с 1.
+    begin
+      if CompareStr('+', LeftStr(Memo1.Lines[i], 1)) = 0 then
+          //begin
+          //  arr[n]:=i; n:=n+1;// порядковый номер следующего.
+          //end;
+          Memo1.Lines.Delete(i);
+    end;
+end;
+
+procedure TForm1.Button9Click(Sender: TObject);
+var
+  MyURL: String;
+begin
+  MyURL := 'http://csn.net4me.net';
+  //ShellExecute(Application.Handle, PChar('open'), PChar(MyLink), nil, nil, SW_SHOW);
+  OpenURL(MyURL);
+end;
+
+
+procedure TForm1.Button6Click(Sender: TObject); // Поддержка.
+var
+   S: String;
+begin
+     Memo1.Clear;
+     S := 'Привет родной, любимый мой, ты уже молодец!'+LineEnding+'Я рад тебе и верю в тебя. Сейчас, здесь.'+LineEnding+'Ты очень нужен мне. Нужен. Любой.'+LineEnding+LineEnding+'У тебя получится всё чему должно получиться. Обязательно!'+LineEnding+'Ты же такой умничка!'+LineEnding+'Обнимаю тебя, мой родной.'+LineEnding;
+     Memo1.Text:= S;
+end;
+
+// END
+end.
+
